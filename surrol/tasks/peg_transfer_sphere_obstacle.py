e13c9a9bc544f125f59062ff1fba7e76949e7569
KANTAPHAT LEELAKUNWET521370990017 <kantaphat.lee@sjtu.edu.cn>
2025-01-07 17:06:27 +0800

    feat: adjust obstacle position

--- surrol/tasks/peg_transfer_sphere_obstacle.py
+++ surrol/tasks/peg_transfer_sphere_obstacle.py
@@ -89,7 +89,7 @@ class PegTransfer(PsmEnv):
         # Reset obstacle position (constant so far)
         p.resetBasePositionAndOrientation(
             self.obj_ids['obstacle'][0],
-            np.array([2.8307116, -0.1153302, 3.67863274]),
+            np.array([self.goal[0], self.goal[1] - 0.1, self.goal[2] + 0.08]),
             (0, 0, 0, 1))
         
         self._waypoints = [None, None, None, None, None, None]  # six waypoints
